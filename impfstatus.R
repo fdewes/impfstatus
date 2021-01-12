641ea2a4450bc94e21d02858d067b5b74e1e6c16
Florian Dewes <fdewes@gmail.com>
2021-01-12 10:19:03 +0100

    Update impfstatus.R

--- impfstatus.R
+++ impfstatus.R
@@ -2,7 +2,7 @@ library(readxl)
 library(knitr)
 library(dplyr)
 
-print("Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx"
+print("Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx")
 
 download.file("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile", "~/tmp.xlsx")
 t = read_excel("~/tmp.xlsx", sheet = 2)
