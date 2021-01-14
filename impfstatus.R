a9f49b60b9e0835a6fddae48693e1f3cd65939f3
Florian Dewes <fdewes@gmail.com>
2021-01-14 14:07:31 +0100

    Update impfstatus.R

--- impfstatus.R
+++ impfstatus.R
@@ -2,10 +2,14 @@ library(readxl)
 library(knitr)
 library(dplyr)
 
-print("Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx")
 
 download.file("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile", "~/tmp.xlsx")
-t = read_excel("~/tmp.xlsx", sheet = 2)
-info = read_excel("~/tmp.xlsx", sheet = 1)
+t <- read_excel("~/tmp.xlsx", sheet = 2)
+info <- read_excel("~/tmp.xlsx", sheet = 1)
+
+t <- t %>% filter(!is.na(Bundesland)) %>% select(Bundesland, starts_with("Impf"), starts_with("Diff"))
+t[,3] = round(t[,3],1)
+
+print("Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx")
 kable(info[2,])
-kable(t %>% filter(!is.na(Bundesland)) %>% select(Bundesland, starts_with("Impf"), starts_with("Diff")))
+kable(t)
