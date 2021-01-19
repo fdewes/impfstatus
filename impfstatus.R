3a56ee41b00a71ef4e80ee300ddeb8851f62f2cd
Florian Dewes <fdewes@gmail.com>
2021-01-19 14:21:48 +0100

    Update impfstatus.R

--- impfstatus.R
+++ impfstatus.R
@@ -10,10 +10,10 @@ info <- read_excel("~/tmp.xlsx", sheet = 1)
 
 #t <- t %>% filter(!is.na(Bundesland)) %>% select(Bundesland, starts_with("Impf"), starts_with("Diff"))
 #t[,3] = round(t[,3],1)
+#t$...7 <- round(as.double(t$...7), 1)
 
 print("Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx")
 kable(info[2,])
 
-t$...7 <- round(as.double(t$...7), 1)
-t <- t %>% rename("Impfquote %" = "...7")
+t <- t %>% select(-RS)
 kable(t,digits=1)
